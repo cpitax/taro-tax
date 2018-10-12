e0fb9f8d77ff8fc8add4039818893d1ca9194759
Terry Cai <gidcai@gmail.com>
2018-10-12 20:16:46 +0800

    input样式统一

---
+++ deploy/web.sh
@@ -0,0 +1,15 @@
+
+#  local deploy web
+# sh ./deploy/web.sh
+
+export TRAVIS_COMMIT_MSG="[deploy] $(git log --format='%h - %B' --no-merges -n 1)"
+mkdir temp_web
+
+git clone --depth 1 -b master --single-branch https://github.com/cpitax/cpitax.github.io.git temp_web
+
+cp -rf dist/**  temp_web/
+
+cd temp_web
+git add -A .
+git commit -m "$TRAVIS_COMMIT_MSG"
+git push origin master
